9e900ef75b5f0e5f50fcc77b250cf26c01742aaa
Sujithmalga <109858061+Sujithmalga@users.noreply.github.com>
2024-05-20 13:04:10 +0530

    Update input.tf

--- input.tf
+++ input.tf
@@ -1,35 +1,48 @@
 variable "primary" {
-    type = list(string)
-    description = " create vnet"
-    default = [ "192.162.0.0/16" ]   
+  type        = list(string)
+  description = "Create VNet address space"
+  default     = ["192.168.0.0/16"]
 }
+
 variable "subnet1" {
-    type = string
-    description = " create vnet"
-    default = "192.162.0.0/24"    
+  type        = string
+  description = "Create subnet1 address prefix"
+  default     = "192.168.0.0/24"
 }
+
 variable "subnet2" {
-    type = string
-    description = " create subnet2"
-    default =  "192.162.1.0/24"    
+  type        = string
+  description = "Create subnet2 address prefix"
+  default     = "192.168.1.0/24"
 }
 
 variable "virtual_machine" {
-    type = list(object)
-    default = [
-         size                = "Standard_F2"
-         admin_username      = "adminuser"
-         name                = "example-machine"]
-    description = " create virtual machine"  
+  type = object({
+    size           = string
+    admin_username = string
+    name           = string
+  })
+  default = {
+    size           = "Standard_F2"
+    admin_username = "adminuser"
+    name           = "example-machine"
+  }
+  description = "Create virtual machine configuration"
 }
- variable "sshfile" {
-    type = list(object)
-    description = " create sshfile"
-    default = [
-         username   = "adminuser"
-         public_key = file("~/.ssh/id_rsa.pub")
-    ]   
+
+variable "sshfile" {
+  type = object({
+    public_key = string
+  })
+  description = "SSH public key for the virtual machine"
 }
-   
+
+variable "publicip" {
+  type = object({
+    name              = string
+    allocation_method = string
+  })
+  description = "Create public ip configuration"
+}
 
 
